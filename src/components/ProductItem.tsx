e80cb3e085cb1c6ac69604387eb40ff859e0893b
Geraldo Dutra Neto <geraldoaax@gmail.com>
2022-06-07 22:40:30 -0300

    memo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,26 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+/**
+ * 1. Criar uma nova versao do componente
+ * 2. Compara com a versao anterior
+ * 3. se tiver alteracoes, atualiza com o que alterou
+ * //shallow compare = verifica a igualdade das informações dentro das propriedades
+ * {} === {} = false = Igualdade referencial --- compara se estao ocupando o mesmo
+ * lugar na memória
+ */
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps, nextProps);
+  }
+);
